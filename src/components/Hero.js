bfa4366ef7b7174992cb3bec850911b75e6e81c4
avenged7fol <59294896+cgrgnc@users.noreply.github.com>
2021-12-19 09:53:46 +0100

    finalized

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,54 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import "../styles/hero.scss";
 import { Link as Linkt } from "react-scroll";
 import cameraicon from "../assets/camera.png";
 import Slider from './Slider';
 import Slider2 from './Slider2';
-import image1 from "../assets/1-guillermo-mota.jpeg";
-import image2 from "../assets/2-pawel-czerwinski.jpeg";
-import image3 from "../assets/3-xavier-coiffic.jpeg";
-import image4 from "../assets/4-mike-van-den-bos.jpeg";
-import image5 from "../assets/5-michael-mouritz.jpeg";
-import image6 from "../assets/6-matt-seymour.jpeg";
-import image7 from "../assets/7-paul-gaudriault.jpeg";
-import image8 from "../assets/8-rodion-kutsaev.jpeg";
-
-
-
-
+import axios from "axios";
 
 const Hero = (props) => {
-    const artists = [
-        {
-        name: "Guillermo Mota",
-        },
-        {
-            name: "Pawel Czerwinski",
-        },
-        {
-            name: "Xavier Coiffic",
-        },
-        {
-            name: "Mike van den Bos",
-        },
-        {
-            name: "Micheal Mouritz",
-        },
-        {
-            name: "Matt Seymour",
-        },
-        {
-            name: "Paul Gaudriault",
-        },
-        {
-            name: "Rodion Kutsaev",
-        }
-    
-    ]
 
-    const handleSubmit=()=>{
-
+    const handleSubmit=(e)=>{
+       
     }
+    const id = process.env.REACT_APP_API_KEY;
+
+    const [imgurls, setImgurls] = useState([]);
+    const [authors, setAuthors] = useState([]);
+
+    
+    useEffect(()=>{
+        axios.get(`https://api.unsplash.com/collections/dddGXhnfvTo/photos/?query=''&client_id=${id}`).then(res=>{
+            console.log(res.data);
+            res.data.map(item=>{
+                setImgurls(imgurls => [...imgurls, item.urls.regular]);
+                setAuthors(authors => [...authors, item.user.name]);
+            })
+        
+        })
+    },[])
     
 
     return (
@@ -64,21 +42,28 @@ const Hero = (props) => {
                 <div className="title2 head">Photographer</div>
                 <div className="text head">My beautiful collection</div>
                 <img src={cameraicon} alt="ico" className='cameraicon' />
-                <Slider image1={image1} artist1={artists[0].name} image2={image2} artist2={artists[1].name} image3={image3} artist3={artists[2].name} image4={image4} artist4={artists[3].name}/>
-                
+                {(()=>{
+                    if(imgurls.length > 0){
+                        return(<Slider image1={imgurls[0]} artist1={authors[0]} image2={imgurls[1]} artist2={authors[1]} image3={imgurls[2]} artist3={authors[2]} image4={imgurls[3]} artist4={authors[3]}/>)
+                    }
+                })()}
                 <div className="center">
                     <div className="title">photogprahy</div>
                     <div className="title2">Leidenschaft</div>
                     <div className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
                 </div>
-                <Slider2 image1={image5} artist1={artists[4].name}  image2={image6} artist2={artists[5].name}  image3={image7} artist3={artists[6].name}  image4={image8} artist4={artists[7].name} />
+                {(()=>{
+                    if(imgurls.length > 0){
+                        return(<Slider2 image1={imgurls[4]} artist1={authors[4]}  image2={imgurls[5]} artist2={authors[5]}  image3={imgurls[6]} artist3={authors[6]}  image4={imgurls[7]} artist4={authors[7]} />)
+                    }
+                })()}
+                
 
             </div>
             <div className="back-center" id="back-center">
                 <div className="center">
-                        {/* <div className="title">photogprahy</div> */}
                         <div className="title2">Contact</div>
-                        <form onSubmit={handleSubmit} action="https://formsubmit.co/cagri.genc@yandex.com" method='POST'>
+                        <form onSubmit={handleSubmit} action="https://formspree.io/f/xbjwjbok" method='POST'>
                             <div className="envelop">
                                 <i className="far fa-envelope"></i>
                             </div>
